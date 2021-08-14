32c169ae0cce638c436a7db3b72ca146a9794aac
AugustoFKL <augusto.fotino@hotmail.com>
2021-08-14 14:46:31 -0300

    #noissue fixed untracked files

---
+++ test/visao/ButtonFieldTest.java
@@ -0,0 +1,14 @@
+package visao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ButtonFieldTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
